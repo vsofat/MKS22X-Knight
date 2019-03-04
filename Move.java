84302b6626ecd0b77602b2af9aca3b661b4a99b8
vsofat <vsofat00@stuy.edu>
2019-03-04 13:39:34 -0500

    moved class to diff file

---
+++ Move.java
@@ -0,0 +1,28 @@
+public class move{
+
+  int row;
+  int col;
+  int possibleMoves;
+
+  public move(int rowInput, int colInput){
+    row = rowInput;
+    col = colInput;
+  }
+
+  public int getCol(){
+    return this.col;
+  }
+
+  public int getRow(){
+    return this.row;
+  }
+
+  public int getPossibleMoves(){
+    return possibleMoves;
+  }
+
+  public void setPossibleMoves(int possibleMoves){
+    this.possibleMoves = possibleMoves;
+  }
+
+}
